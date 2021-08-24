91dc22ef0ad600a4ea6e6408b65a2597bd91ba74
David Loerinci <david.loerinci@gmail.com>
2021-08-24 17:11:17 +0200

    added new line

--- Test_001/test_001_build_mail_address.py
+++ Test_001/test_001_build_mail_address.py
@@ -8,6 +8,7 @@ from Source.build_mail_address import BuildMailAddress
 
 config = GetInput(__file__)
 
+
 class TestClass:
     def test_001_build_mail_address(self):
         test_code_file = os.path.join(
