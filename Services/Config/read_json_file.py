f647f5867cf2291f3975a6ba4158aac7b00b36fe
David Loerinci <david.loerinci@gmail.com>
2021-08-26 12:07:01 +0200

    updated read_jspn_file

--- Services/Config/read_json_file.py
+++ Services/Config/read_json_file.py
@@ -2,29 +2,14 @@ import json
 import os
 
 
-class ReadJsonFile(object):
-    @staticmethod
+class ReadJsonFile:
     def is_json_path_valid(json_path):
         is_valid = os.path.exists(json_path)
         is_valid &= os.path.isfile(json_path)
         return is_valid
 
-    @staticmethod
-    def load(json_path, key=""):
+    def load(json_path):
         if ReadJsonFile.is_json_path_valid(json_path):
-            try:
-                with open(json_path, "r") as json_path:
-                    inputs = json.load(json_path)
-                    if key == "":
-                        return inputs
-                    elif key in inputs:
-                        return inputs[key]
-                    else:
-                        print('No key in configuration file')
-                        return {}
-            except:
-                print('Check configuration file for formatting errors')
-                return {}
-        else:
-            print('Could not open configuration file')
-            return {}
+            with open(json_path, "r") as json_path:
+                inputs = json.load(json_path)
+                return inputs
