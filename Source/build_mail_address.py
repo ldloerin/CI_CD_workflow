65a0cd95002727528f5248668ad5ef1b0f838efe
David Loerinci <david.loerinci@gmail.com>
2021-08-27 21:15:02 +0200

    added line

--- Source/build_mail_address.py
+++ Source/build_mail_address.py
@@ -17,5 +17,6 @@ class BuildMailAddress(GetInput):
         self.mail_address = self.mail_address.lower()
         print(self.mail_address)
 
+
 my_code = BuildMailAddress(__file__)
 my_code.execute_workflow()
