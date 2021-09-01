5e4c0a9147393fac30f372220df45e3287dfadc5
David Loerinci <david.loerinci@gmail.com>
2021-09-01 18:20:59 +0200

    removed obsolete lines

--- TestServices/Config/get_test_input.py
+++ TestServices/Config/get_test_input.py
@@ -7,13 +7,6 @@ class GetTestInput:
         self.code_path = os.path.dirname(main_code_file)
         self.root_path = os.path.dirname(self.code_path)
         self.__get_input_parameters()
-        try:
-            self.__get_file_path('main_source_code')
-            self.__get_file_path('general_hashtag_file')
-            self.__get_file_path('essential_hashtag_file')
-            self.__get_file_path('output_hashtag_file')
-        except Exception as ex:
-            print(ex)
 
     def __get_input_parameters(self):
         parameter_file = os.path.join(self.code_path, "Input", "config.json")
@@ -21,9 +14,3 @@ class GetTestInput:
         inputs = ReadTestJsonFile.load(parameter_file)
         for key, value in inputs.items():
             setattr(self, key, value)
-
-    def __get_file_path(self, key):
-        relative_path = getattr(self, key)
-        file_path = os.path.join(self.root_path, relative_path)
-        file_path = file_path.replace('\\', '/')
-        setattr(self, key, file_path)
